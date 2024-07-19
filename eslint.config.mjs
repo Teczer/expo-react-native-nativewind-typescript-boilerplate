0352bbdce8aa710ccbc6e5d28fe16754f8d1f716
Teczer <mehdi.hattou1@gmail.com>
2024-07-19 16:38:13 +0200

    :lipstick: Prettier : Add prettier configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 import { fixupConfigRules } from "@eslint/compat";
+import configPrettier from "eslint-config-prettier";
 
 export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
@@ -18,15 +19,18 @@ export default [
   ...tseslint.configs.recommended,
   ...fixupConfigRules(pluginReactConfig),
   {
+    plugins: ["prettier"],
     rules: {
-      "react/react-in-jsx-scope": "off",
+      "prettier/prettier": "error", // Ajoute une règle ESLint pour Prettier
+      "react/react-in-jsx-scope": "off", // Déjà configuré
     },
   },
   {
     settings: {
       react: {
-        version: "detect",
+        version: "detect", // Détecte automatiquement la version de React
       },
     },
   },
+  ...configPrettier, // Assure que les règles ESLint n'entrent pas en conflit avec Prettier
 ];
