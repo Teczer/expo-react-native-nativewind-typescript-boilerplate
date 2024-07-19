1aaf63833b373f0776470af66c47ddf2eed53c38
Teczer <mehdi.hattou1@gmail.com>
2024-07-19 19:04:27 +0200

    :wrench: Test : Add default test

---
+++ __tests__/init.test.ts
@@ -0,0 +1,6 @@
+import { test, expect } from "@jest/globals"; // Import test from Jest
+
+test("sample test", () => {
+  // write your test logic here
+  expect(1 + 2).toBe(3); // Example test logic
+});
